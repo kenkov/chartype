f5c29891794540e9c7f685af6f128142456b1b8a
Noriyuki Abe <kenko.py@gmail.com>
2013-04-28 11:59:41 -0700

    first commit

---
+++ setup.py
@@ -0,0 +1,14 @@
+#! /usr/bin/env python
+# coding:utf-8
+
+from setuptools import setup
+
+setup(
+    name="chartype",
+    version="0.1",
+    description="Check character types",
+    author="Noriyuki Abe",
+    author_email="kenko.py@gmail.com",
+    url="http://kenkov.jp",
+    py_modules=["chartype"]
+)
