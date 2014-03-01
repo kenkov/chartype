e6d5305e334dfe5e298ea1c0fecdce24602e05bc
Noriyuki Abe <kenko.py@gmail.com>
2014-03-02 05:03:34 +0900

    Implements methods for conversion
    -   hiragana2katakana
    -   katakana2hiranaga
    -   half2full
    -   full2half

--- chartype.py
+++ chartype.py
@@ -18,6 +18,27 @@ Unicode 型の文字列がどの種類の文字列(ひらがな、カタカナ�
 '''
 
 import unicodedata
+import re
+
+
+class CharException(TypeError):
+
+    def __init__(self, sent):
+        self.sent = sent
+
+    def __str__(self):
+        return "need a single Unicode character as parameter\
+: {}".format(self.sent)
+
+
+class CharTypeException(TypeError):
+
+    def __init__(self, sent):
+        self.sent = sent
+
+    def __str__(self):
+        return "need a halfwidth katakana character\
+as a parameter: {}".format(self.sent)
 
 
 class Chartype(object):
@@ -55,10 +76,15 @@ class Chartype(object):
         elif self.is_touten(st):
             return 'TOUTEN'
 
+
     def _is_type(self, typ, st, start, end):
         '''
         '''
-        return unicodedata.name(st)[start: end] == typ
+        try:
+            return unicodedata.name(st)[start: end] == typ
+        except TypeError as ex:
+            raise CharException(st) from ex
+
 
     def is_hiragana(self, st):
         u"""
@@ -311,6 +337,42 @@ class Chartype(object):
         True
         '''
         return self._typename(st1) == self._typename(st2)
+
+
+    def hiragana2katakana(self, char):
+        """ひらがなをカタカナに変換する"""
+        if not self.is_hiragana(char):
+            raise CharTypeException(char)
+
+        name = re.sub(r"^HIRAGANA\s", "KATAKANA ", unicodedata.name(char))
+        return unicodedata.lookup(name)
+
+
+    def katakana2hiragana(self, char):
+        """カタカナを平仮名に変換する"""
+        if not self.is_katakana(char):
+            raise CharTypeException(char)
+
+        name = re.sub(r"^KATAKANA\s", "HIRAGANA ", unicodedata.name(char))
+        return unicodedata.lookup(name)
+
+
+    def half2full(self, char):
+        u"""半角カタカナ char を全角カタカナに変換する"""
+        if not self.is_halfwidthkatakana(char):
+            raise CharTypeException(char)
+
+        name = re.sub(r"^HALFWIDTH\s", "", unicodedata.name(char))
+        return unicodedata.lookup(name)
+
+
+    def full2half(self, char):
+        u"""全角カタカナ char を半角カタカナに変換する"""
+        if not self.is_katakana(char):
+            raise CharTypeException(char)
+
+        name = unicodedata.name(char)
+        return unicodedata.lookup("HALFWIDTH " + name)
 
 
 if __name__ == '__main__':
